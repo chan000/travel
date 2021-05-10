e915415a456faeca7c75e405a060fbf3fd02c8bb
chan000 <trupokc961@naver.com>
2021-05-10 16:59:49 +0900

    서울 부분 수정

---
+++ src/main/java/org/ict/controller/SeoulRestController.java
@@ -0,0 +1,10 @@
+package org.ict.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/city/*")
+public class SeoulRestController {
+
+}
---
+++ src/main/java/org/ict/domain/SeoulVO.java
@@ -0,0 +1,11 @@
+package org.ict.domain;
+
+import lombok.Data;
+
+@Data
+public class SeoulVO {
+	private int sno; 
+	private String sarea;
+	private String spost;
+	private int stype;
+}
