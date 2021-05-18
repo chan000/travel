c66f1d1a1cdb0570ace264025ecebb93bd9085c4
chan000 <trupokc961@naver.com>
2021-05-18 15:37:26 +0900

    로그인페이지

--- src/main/java/org/ict/controller/TourRestController.java
+++ src/main/java/org/ict/controller/TourRestController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.log4j.Log4j;
 
 @RestController
-@Log4j
 @RequestMapping("/board/*")
 public class TourRestController {
 
--- src/main/java/org/ict/controller/UserRestController.java
+++
@@ -1,5 +0,0 @@
-package org.ict.controller;
-
-public class UserRestController {
-
-}
